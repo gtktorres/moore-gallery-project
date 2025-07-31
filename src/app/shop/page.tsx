94b193f1cd5f149d40e2f80b2201994ff3ea6929
Guevara Torres <gtktorres@gmail.com>
2025-07-31 13:00:57 -0600

    US5 updates art imports

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,20 +2,20 @@
 //import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Art0 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art1 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art2 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art3 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
+import Art1 from '../../components/Project-Images/Connie Art/8E30BA39-0E48-4722-B812-38FF2D3E7844.jpeg';
+import Art2 from '../../components/Project-Images/Connie Art/394BEFEB-8975-4D43-8364-2F25DFD153C9.jpeg';
+import Art3 from '../../components/Project-Images/Connie Art/8669A082-06CB-49CF-95AC-E6453B45F858.jpeg';
 import Art4 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art5 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art6 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art7 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art8 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art9 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art10 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art11 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art12 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
+import Art5 from '../../components/Project-Images/Connie Art/973454FB-50C8-4DA7-8E5A-0A403BF446A5.jpeg';
+import Art6 from '../../components/Project-Images/Connie Art/B9FC5A7C-AD20-4967-AD49-824410F7FC22.jpeg';
+import Art7 from '../../components/Project-Images/Connie Art/BF2A9F2A-07C9-46BE-A528-BBA7D25B5E7A.jpeg';
+import Art8 from '../../components/Project-Images/Connie Art/C204BD0D-42D5-4F44-89E0-D1B6A36F8562.jpeg';
+import Art9 from '../../components/Project-Images/Connie Art/CCEF7CEC-BCFB-4139-9C37-F2380B69A5C7.jpeg';
+import Art10 from '../../components/Project-Images/Connie Art/E0E4B70C-ADF8-4CEA-830B-EF3479602C81.jpeg';
+import Art11 from '../../components/Project-Images/Connie Art/E9166E15-D139-43A5-9911-F88B87C5367E.jpeg';
+import Art12 from '../../components/Project-Images/Connie Art/FB285C43-8846-41D4-A87A-61DF535F1430.jpeg';
 import Art13 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
-import Art014 from '../../components/Project-Images/Connie Art/1E239547-B299-4780-9949-2BC2B4664FD9.jpeg';
+import Art014 from '../../components/Project-Images/Connie Art/8E30BA39-0E48-4722-B812-38FF2D3E7844.jpeg';
 
 const products = [
   {
